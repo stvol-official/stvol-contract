819761b70d9ef47d3e2914dc45eb31a57a99c793
andrew <andrew@hypercape.com>
2024-03-05 18:27:29 +0900

    WIP

--- blast.dev.arguments.js
+++ blast.dev.arguments.js
@@ -1,12 +1,12 @@
 // for blast sepolia
-const usdc = '0xe722424e913f48bAC7CD2C1Ae981e2cD09bd95EC';
+const usdc = '0xaDbd87F5960Be91cCE1d7D5A0a66b36d316C199c';
 const oracle = '0xA2aa501b19aff244D90cc15a4Cf739D2725B5729'; // PythContractAddress
 const admin = '0xC61042a7e9a6fe7E738550f24030D37Ecb296DC0';
 const operator = '0xC61042a7e9a6fe7E738550f24030D37Ecb296DC0';
 const operatorVault = '0xC61042a7e9a6fe7E738550f24030D37Ecb296DC0';
 const commissionFee = 200; // 2%
-const priceId = "0xff61491a931112ddf1bd8147cd1b641375f79f5825126d665480874634fd0ace"; // ETH/USD
-// const priceId = "0xe62df6c8b4a85fe1a67db44dc12de5db330f7ac66b72dc658afedf0f4a415b43"; // BTC/USD
+// const priceId = "0xff61491a931112ddf1bd8147cd1b641375f79f5825126d665480874634fd0ace"; // ETH/USD
+const priceId = "0xe62df6c8b4a85fe1a67db44dc12de5db330f7ac66b72dc658afedf0f4a415b43"; // BTC/USD
 
 module.exports = [
     usdc,
